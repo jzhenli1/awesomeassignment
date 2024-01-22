675f44618a9922868394fdc9b47c471372069794
Jiazhen Li <jiazhen.li@student.uni-tuebingen.de>
2024-01-22 20:57:15 +0100

    updated pvals function

--- R/pvals.R
+++ R/pvals.R
@@ -7,15 +7,19 @@
 #'
 #' @param model An `evaluCATE` object/model considered
 #' @param col_name A string object of how the column of p-values should be called
-#' @param rownames A list of row names to be plotted (from the evaluCATE package)
 #'
 #' @return A vector of p-values
 #' @export
 #'
 #' @examples
-#' pvals(evaluation_tl, "tl_pval", rownames)
+#' pvals(evaluation_tl, "tl_pval")
 
-pvals <- function(model, col_name, rownames=rownames) {
+pvals <- function(model, col_name) {
+
+  # Define the row names
+  rownames <- c("wr_none", "wr_cddf1", "wr_cddf2", "wr_mck1", "ht_none",
+                "ht_cddf1", "ht_cddf2", "ht_mck1", "ht_mck2", "ht_mck3", "aipw")
+
   # Initialize a matrix to store the results
   pval_mat <- matrix(NA, nrow = length(rownames), ncol = 1)
   rownames(pval_mat) <- rownames
